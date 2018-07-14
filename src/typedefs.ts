611e39906b44b02a46cc73aa0bbc75d49db80b96
Ovyerus <iamovyerus@gmail.com>
2018-07-14 22:12:05 +1000

    Add typedefs for deferred promises, and other stuff for awaitMessage

--- src/typedefs.ts
+++ src/typedefs.ts
@@ -2,11 +2,23 @@ import {ClientOptions} from 'eris';
 
 export type MiddlewareHandler = (eventName: string, , ...eventArgs: any[]) => Error?;
 
+export interface ErisaOptions {
+    erisOptions: ClientOptions;
+}
+
 export interface AwaitMessageOptions {
     timeout: number;
     filter(msg: Eris.Message): boolean;
 }
 
-export interface ErisaOptions {
-    erisOptions: ClientOptions;
+export interface DeferredPromise {
+    promise: Promise;
+    resolve(value?: any): Promise<any>;
+    reject(error?: any): Promise<any>;
+}
+
+export interface AwaitingObject {
+    p: DeferredPromise;
+    timer: NodeJS.Timer;
+    filter(msg: Eris.Message): boolean;
 }
