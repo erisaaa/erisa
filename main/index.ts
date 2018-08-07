11a52f06928c82ca5f64341366cf305b7086f791
Ovyerus <iamovyerus@gmail.com>
2018-08-07 16:59:29 +1000

    refactor: revert to es6 style export due to problems with interoperability

--- main/index.ts
+++ main/index.ts
@@ -1,15 +1,2 @@
-import {Erisa} from './src/Erisa';
-import {ErisaOptions} from './src/types';
-
-// Exports stuff similar to Eris (exports main function that creates client, which has client and other stuff as properties).
-interface ErisaExport {
-    (token: string, options: ErisaOptions): Erisa;
-    Erisa: typeof Erisa;
-}
-
-const _Erisa: ErisaExport = ((token: string, options: ErisaOptions = {}): Erisa =>
-    new Erisa(token, options)) as ErisaExport;
-
-_Erisa.Erisa = Erisa;
-
-export = _Erisa;
+export {Erisa} from './src/Erisa';
+export {AwaitTimeout, ErisaOptions} from './src/types';
