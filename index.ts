9066339c5b3dc78449354884c5bae2d7b3bd4271
Ovyerus <iamovyerus@gmail.com>
2019-11-12 12:27:36 +1100

    feat: change exports yet again

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
-export {default} from './src/Erisa';
+import Erisa from './src/Erisa';
+
+export {default as Erisa} from './src/Erisa';
 export * from './src/types';
 export { FormatError } from './src/util';
+export default Erisa;
