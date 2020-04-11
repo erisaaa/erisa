443d909349831c33bc3a8b420a59a7ea58fb98ef
Ovyerus <iamovyerus@gmail.com>
2020-04-11 12:29:12 +1000

    feat: export Eris types

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
-export {default, default as Erisa} from './src/Erisa';
+export { default, default as Erisa } from './src/Erisa';
 export * from './src/types';
 export { FormatError } from './src/util';
+export * from 'eris';
